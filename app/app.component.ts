c6365f62645cd634325a40f0fd8757651baa1845
kiralyste <kiralyste@gmail.com>
2017-11-11 21:54:38 +0100

    Template fundamentals
    Interpolation and expressions

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,15 +5,26 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      {{ title }}
+      {{ title + '!'}}
+      <div>
+        {{ numberOne }}
+        {{ numberTwo}}
+      </div>
+      <div>
+        {{ numberOne ++ numberTwo}}
+      </div>
+      <div>
+        {{ idHappy ? ':)' : ':('}}
+      </div>
     </div>
   `
 })
 
 export class AppComponent {
-  
   title: string;
-  
+  isHappy: boolean = true;
+  numberOne: number = 1;
+  numberTwo: number = 2;
   constructor() {
     this.title = 'Ultimate Angular'
   }
