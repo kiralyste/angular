a9b800ebc187cec3ad764d3e08cee29fa90e6371
kiralyste <kiralyste@gmail.com>
2017-11-11 23:03:51 +0100

    Property binding
    Example 2

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,12 +7,17 @@ import { Component } from '@angular/core';
     <div class="app">
       <h1>{{ title }}</h1>
       <h1 [innerHTML]="title"></h1>
+      <img [src]="avatar">
+      <input type="text" [value]="name">
+      <div> {{ name }} </div> 
     </div>
   `
 })
 
 export class AppComponent {
   title: string;
+  name: string = 'Steven';
+  avatar: string = 'img/avatar.png';
   constructor() {
     this.title = 'Ultimate Angular'
   }
