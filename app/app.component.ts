dd7391a53e2330861ded63d7b5f0fff353807fc8
kiralyste <kiralyste@gmail.com>
2017-11-11 22:23:53 +0100

    Property binding
    Example 1

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,26 +5,14 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      {{ title + '!'}}
-      <div>
-        {{ numberOne }}
-        {{ numberTwo}}
-      </div>
-      <div>
-        {{ numberOne ++ numberTwo}}
-      </div>
-      <div>
-        {{ idHappy ? ':)' : ':('}}
-      </div>
+      <h1>{{ title }}</h1>
+      <h1 [innerHTML]="title"></h1>
     </div>
   `
 })
 
 export class AppComponent {
   title: string;
-  isHappy: boolean = true;
-  numberOne: number = 1;
-  numberTwo: number = 2;
   constructor() {
     this.title = 'Ultimate Angular'
   }
