55cf29de0382d8a605b3e7535fdff42a1035ed49
kiralyste <kiralyste@gmail.com>
2017-11-12 01:12:27 +0100

    Prepare
    Prepare for event binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,9 +5,6 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      <h1>{{ title }}</h1>
-      <h1 [innerHTML]="title"></h1>
-      <img [src]="avatar">
       <input type="text" [value]="name">
       <div> {{ name }} </div> 
     </div>
@@ -15,10 +12,8 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  title: string;
-  name: string = 'Steven';
-  avatar: string = 'img/avatar.png';
+  name: string;
   constructor() {
-    this.title = 'Ultimate Angular'
+    this.name = 'Steven';
   }
 }
