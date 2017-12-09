3b411bf0d839a9c0022e62e7a717f22c072e15db
kiralyste <kiralyste@gmail.com>
2017-12-09 14:31:17 +0100

    Rendering flows
    - ngFor
    - Interface

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,37 +1,55 @@
 import { Component } from '@angular/core';
 
+interface Passenger {
+  id: number,
+  fullname: string,
+  checkedIn: boolean
+}
+
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      <input 
-        type="text"
-        [value]="name"
-        (input)="handleChange($event.target.value)">
-      
-        <template [ngIf]="name.length > 2">
-          Searching for ... {{ name }}
-        </template>
-      
-        <div *ngIf="name.length > 2"> 
-          Searching for ... {{ name }}
-        </div> 
+      <h3>Airline Passengers</h3>
+      <h2>{{ passengers[0].fullname }}</h2>  
     </div>
   `
 })
 
 export class AppComponent {
-  name: string;
+  passengers: Passenger[];
   constructor() {
-    this.name = '';
+    this.passengers = [{
+      id: 1,
+      fullname: 'Stephen',
+      checkedIn: true
+    }, {
+      id: 2,
+      fullname: 'Rose',
+      checkedIn: false
+    }, {
+      id: 3,
+      fullname: 'James',
+      checkedIn: true
+    }, {
+      id: 4,
+      fullname: 'Louise',
+      checkedIn: true
+    }, {
+      id: 5,
+      fullname: 'Tina',
+      checkedIn: false
+    }];
   }
+
+
+
   handleClick(value: string) {
     console.log(value);
   }
 
   handleChange(value: string) {
-    this.name = value;
     console.log(value);
   }
 }
