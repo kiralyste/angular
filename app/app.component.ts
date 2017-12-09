937248bcb10f9216433bf1db006455611c34374b
kiralyste <kiralyste@gmail.com>
2017-12-09 21:50:39 +0100

    Rendering flows
    Pipes for data transformation
    - JSON-Pipe
    - Date-Pipe
    - Uppercase-Pipe

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 interface Passenger {
   id: number,
   fullname: string,
-  checkedIn: boolean
+  checkedIn: boolean,
+  checkInDate: number | null
 }
 
 @Component({
@@ -11,7 +12,7 @@ interface Passenger {
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      <h3>Airline Passengers</h3>
+      <h3>Airline Passengers ()</h3>
       <ul>
         <li *ngFor="let passenger of passengers; let i = index;">
           <span 
@@ -52,6 +53,22 @@ interface Passenger {
        {{ i }}: {{ passenger.fullname }}
       </li>
       </ul>
+      <h3>Airline Passengers (with ngStyle and JSON-Pipes) </h3>
+      <ul>
+      <li *ngFor="let passenger of passengers; let i = index;">
+        <span 
+          class="status"
+          [ngStyle]="{
+            backgroundColor: (passenger.checkedIn ? '#2ecc71' : '#c0392b')
+          }"></span> 
+       {{ i }}: {{ passenger.fullname }}
+       <p>{{ passenger | json }}</p>
+       <div class="date">
+          Check in date:
+            {{ passenger.checkInDate ? (passenger.checkInDate | date: 'yMMMMd' | uppercase) : 'Not checked in' }}
+       </div>
+      </li>
+      </ul>
     </div>
   `
 })
@@ -62,23 +79,28 @@ export class AppComponent {
     this.passengers = [{
       id: 1,
       fullname: 'Stephen',
-      checkedIn: true
+      checkedIn: true,
+      checkInDate: 1490742000000
     }, {
       id: 2,
       fullname: 'Rose',
-      checkedIn: false
+      checkedIn: false,
+      checkInDate: null
     }, {
       id: 3,
       fullname: 'James',
-      checkedIn: true
+      checkedIn: true,
+      checkInDate: 1491606000000
     }, {
       id: 4,
       fullname: 'Louise',
-      checkedIn: true
+      checkedIn: true,
+      checkInDate: 1488412800000
     }, {
       id: 5,
       fullname: 'Tina',
-      checkedIn: false
+      checkedIn: false,
+      checkInDate: null
     }];
   }
 
