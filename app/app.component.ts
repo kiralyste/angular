44338c47572c045a57717318608947aa497c6a5e
kiralyste <kiralyste@gmail.com>
2017-12-10 14:11:31 +0100

    Rendering flows
    - Safe navigation operator
    
    Avoid ErrorMessage in the browser: "can not read property 'length' of null

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+interface Child {
+  name: string,
+  age: number
+}
+
 interface Passenger {
   id: number,
   fullname: string,
   checkedIn: boolean,
-  checkInDate: number | null
+  checkInDate: number | null,
+  children: Child[] | null
 }
 
 @Component({
@@ -69,6 +75,25 @@ interface Passenger {
        </div>
       </li>
       </ul>
+      <h3>Airline Passengers (with ngStyle and JSON-Pipes and safe navigation operator) </h3>
+      <ul>
+      <li *ngFor="let passenger of passengers; let i = index;">
+        <span 
+          class="status"
+          [ngStyle]="{
+            backgroundColor: (passenger.checkedIn ? '#2ecc71' : '#c0392b')
+          }"></span> 
+       {{ i }}: {{ passenger.fullname }}
+       <p>{{ passenger | json }}</p>
+       <div class="date">
+          Check in date:
+            {{ passenger.checkInDate ? (passenger.checkInDate | date: 'yMMMMd' | uppercase) : 'Not checked in' }}
+       </div>
+       <div class="children">
+          children: {{ passenger.children?.length || 0}}
+       </div>
+      </li>
+      </ul>
     </div>
   `
 })
@@ -80,27 +105,33 @@ export class AppComponent {
       id: 1,
       fullname: 'Stephen',
       checkedIn: true,
-      checkInDate: 1490742000000
+      checkInDate: 1490742000000,
+      children: null
     }, {
       id: 2,
       fullname: 'Rose',
       checkedIn: false,
-      checkInDate: null
+      checkInDate: null,
+      children: [{ name: 'Ted', age: 12},
+                  {name: 'Chloe', age: 7}]
     }, {
       id: 3,
       fullname: 'James',
       checkedIn: true,
-      checkInDate: 1491606000000
+      checkInDate: 1491606000000,
+      children: null
     }, {
       id: 4,
       fullname: 'Louise',
       checkedIn: true,
-      checkInDate: 1488412800000
+      checkInDate: 1488412800000,
+      children: [{name: 'Jessica', age: 1}]
     }, {
       id: 5,
       fullname: 'Tina',
       checkedIn: false,
-      checkInDate: null
+      checkInDate: null,
+      children: null
     }];
   }
 
